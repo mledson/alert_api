621b8e9e7fcc810ca578d920a6f31d5a4a241b9b
mledson <miulin.edson@variq.com>
2016-07-11 22:15:30 -0400

    Setting up rspec and versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+require 'api_constraints'
+
 AlertApi::Application.routes.draw do
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+           constraints: ApiConstraints.new(verision: 1, default: true) do
+    end
+
   end
 end
